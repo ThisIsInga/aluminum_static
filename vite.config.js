5be50bb4572ec7f5db1e740e2c964c737e0d38c7
ThisIsInga <thisisingauwu@gmail.com>
2026-08-15 17:40:57 +1000

    first commit

---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/aluminum_static/',
+  server: {
+    port: 3002,
+    host: true,
+    allowedHosts: [
+      'thisistest.ru',
+      'localhost',
+    ],
+    proxy: {
+      '/api': {
+        target: 'https://thisistest.ru/',
+        changeOrigin: true,
+        secure: false,
+      },
+    }
+  }
+})

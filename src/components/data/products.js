cc6f7ec7906055a28148dca29a36c36ee2d8f010
ThisIsInga <thisisingauwu@gmail.com>
2026-09-18 14:03:20 +1000

    перенос данных в глобальный файл

---
+++ src/components/data/products.js
@@ -0,0 +1,39 @@
+import img1 from './../../../public/assets/Sale/sale1.jpg'
+import img2 from './../../../public/assets/Sale/sale2.png'
+import img3 from './../../../public/assets/Sale/sale3.png'
+import img4 from './../../../public/assets/Sale/sale4.png'
+
+export const products = [
+    {
+      id: 1,
+      newPrice: "250 000",
+      oldPrice: "350 000",
+      title: "Вакуумный подъёмник для стекла Итальянский",
+      shortDescription: "Пневматический вакуумный подъемник для листов стекла Elephant",
+      img: img1
+    },
+    {
+      id: 2,
+      newPrice: "6 000",
+      oldPrice: "10 000",
+      title: "Инфракрасный обогревательBallu BIH-T-4.5",
+      shortDescription: "Отличное решение для обогрева больших помещений, цехов, гаражей или открытых площадок",
+      img: img2
+    },
+    {
+      id: 3,
+      newPrice: "2 000",
+      oldPrice: "4 000",
+      title: "Алюминиевые композитные панели",
+      shortDescription: "Идеально подходят для облицовки фасадов, изготовления наружной рекламы и интерьерной отделки",
+      img: img3
+    },
+    {
+      id: 4,
+      newPrice: "2 000",
+      oldPrice: "",
+      title: "СКРЫТЫЕ МОСКИТНЫЕ СЕТКИ",
+      shortDescription: "Скрытые москитные сетки нового поколения!",
+      img: img4
+    }
+  ];
--- src/components/templates/new/NewTemplates.jsx
+++ src/components/templates/new/NewTemplates.jsx
@@ -1,32 +1,35 @@
 import testWindow from './../../../../public/assets/New/test_window.mp4';
 import windowVideo from './../../../../public/assets/New/window.mp4';
+import Card from '../../layout/Card/Card.jsx';
 import style from './NewTemplates.module.css';
 import { Link } from 'react-router-dom';
 
 function New () {
     return(
-       <section className={style.newBackground}>
-            <div className={style.video}>
-                <video 
-                    src={testWindow}
-                    autoPlay
-                    loop
-                    muted
-                />
-            </div>
-            <div className={style.text}>
-                <h2>НОВИНКА</h2>
-                <p>Скрытые москитные сетки</p>
-            </div>
-            <div className={style.video}>
-                <video 
-                    src={windowVideo}
-                    autoPlay
-                    loop
-                    muted
-                />
-            </div>
-       </section> 
+        <Link to="/sale">
+            <section className={style.newBackground}>
+                <div className={style.video}>
+                    <video 
+                        src={testWindow}
+                        autoPlay
+                        loop
+                        muted
+                    />
+                </div>
+                <div className={style.text}>
+                    <h2>НОВИНКА</h2>
+                    <p>Скрытые москитные сетки</p>
+                </div>
+                <div className={style.video}>
+                    <video 
+                        src={windowVideo}
+                        autoPlay
+                        loop
+                        muted
+                    />
+                </div>
+            </section> 
+        </Link>
     )
 }
 
